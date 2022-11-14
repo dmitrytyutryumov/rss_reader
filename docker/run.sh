0daa23b8872d11eb6788fd595761e39c5604eac7
dmitry.tyutryumov <dmitry.tyutryumov@gamil.com>
2022-11-14 12:56:30 +0100

    speed up gunicorn

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-
 poetry run python manage.py migrate
 poetry run python manage.py collectstatic --noinput
-poetry run gunicorn api.wsgi -b 0.0.0.0:8000
+poetry run gunicorn api.wsgi -b 0.0.0.0:8000 --workers=2 --threads=4
--- src/api/api/settings/base.py
+++ src/api/api/settings/base.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.parent / "static"
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR.parent / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
